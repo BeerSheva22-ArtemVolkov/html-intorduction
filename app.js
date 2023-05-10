1299dfab90c89a9aea1db983e1d21a1dd7346615
Art96em <artem-w0lf@yandex.ru>
2023-05-10 11:54:32 +0300

    final commit for primitives & conversions

--- app.js
+++ app.js
@@ -68,24 +68,30 @@
 // Use symbols "A", "S"
 //print out "ananas"
 
-console.log(("A" + +"A" + "AS").toLocaleLowerCase());  // ananas
+// console.log(("A" + +"A" + "AS").toLocaleLowerCase());  // ananas
 
 // console.log(myToStingInt(-260124, 16));
 // console.log((-260124).toString(16));
 
-errors = 0;
-for (radix = 2; radix < 37; radix ++) {
-    for (i = -1000; i < 1001; i++) {
-        if (myToStingInt(i, radix).toLowerCase() != i.toString(radix)) {
-            errors++;
-        }
-    }
-}
-console.log("Errors count " + errors);
+// errors = 0;
+// for (radix = 2; radix < 37; radix ++) {
+//     for (i = -100000; i < 100001; i++) {
+//         if (myToStingInt(i, radix).toLowerCase() != i.toString(radix)) {
+//             errors++;
+//         }
+//     }
+// }
+// console.log("Errors count " + errors);
 
 function myToStingInt(number, radix) {
+
     // number - число
     // radix - система счисления if radix < 2 or radix > 36 then radix = 10
+
+    if (radix < 2 || radix > 36) {
+        radix = 10;
+    }
+
     let isNegative = number < 0;
     number = Math.round(Math.abs(number));
     let res = '';
@@ -95,16 +101,131 @@ function myToStingInt(number, radix) {
         number = Math.floor(number / radix);
     }
     res = getDigit(number) + res;
-    if (isNegative){
-        res = "-" + res;
-    }
-    return res;
+
+    return isNegative ? "-" + res : res;
 }
 
-function getDigit(num){
-    if (num > 9){
-        num = String.fromCharCode(65 + num - 10);
+function getDigit(num) {
+    if (num > 9) {
+        num = String.fromCharCode('A'.charCodeAt(0) + num - 10);
     }
     return num;
 }
 
+function yuriToStingInt(number, radix) {
+    const sign = number < 0 ? '-' : '';
+    number = Math.abs(number);
+    number = Math.round(number);
+
+    if (radix < 2 || radix > 36) {
+        radix = 10;
+    }
+
+    let res = '';
+    do {
+        res = getSymbol(number, radix) + res;
+        number = Math.trunc(number / radix);
+    } while (number != 0);
+    return sign + res;
+}
+
+function getSymbol(number, radix) {
+    const aCode = 'a'.charCodeAt(0);
+    const delta = aCode - 10;
+    const remainder = number % radix;
+    return remainder < 10 ? remainder + '' : String.fromCharCode(remainder + delta);
+}
+
+// console.log((1234567).toString(36));
+// console.log(yuriToStingInt(1234567, 36));
+// console.log(yuriToStingInt(124515.2, 16));
+// console.log(yuriToStingInt(-12345674, 16));
+// console.log(yuriToStingInt(0, 36));
+
+const strNum = '1java';
+// console.log(`string with number ${strNum} with radix 2 is ${parseInt(strNum, 2)}`);
+
+console.log(parseInt("0xff", 16));
+console.log(yuriParseInt("0xff", 16));
+
+function myParseInt(number, radix) {
+    let count = 0;
+    let res = 0;
+    let isNegative = false;
+    number = number.toLocaleLowerCase();
+
+    if (radix < 2 || radix > 36) {
+        radix = 10;
+    }
+
+    if (number.charAt(0) == '-') {
+        isNegative = true;
+        number = number.substring(1);
+    }
+
+    number = checkNumber(number, radix);
+
+    if (number.length != 0) {
+        while (count < number.length) {
+            res += getNum(number.charAt(count)) * (Math.pow(radix, number.length - count++ - 1));
+        }
+        res = isNegative ? -res : res;
+    }
+    else {
+        res = NaN;
+    }
+    return res;
+}
+
+function checkNumber(num, radix) {
+    let res = '';
+    let count = 0;
+    while (count < num.length && getNum(num.charAt(count)) < radix) {
+        res += num.charAt(count++);
+    }
+    return res;
+}
+
+function getNum(ch) {
+    return isNaN(Number(ch)) ? ch.charCodeAt(0) - 87 : ch;
+}
+
+//
+
+function yuriParseInt(strNum, radix) {
+    strNum = strNum.trim();
+    let index = strNum.charAt(0) == '-' || strNum.charAt(0) == '+' ? 1 : 0;
+
+    if ((!radix || radix == 16) && getHexdecimalIndex(strNum.substring(index)) > 0) {
+        index += 2;
+        radix = 16;
+    }
+    if (!radix) {
+        radix = 10;
+    }
+
+    let res = radix > 1 && radix < 37 ? getDigitCode(strNum, index, radix) : NaN;
+    if (!isNaN(res)) {
+        let digit;
+        index++;
+        while (index < strNum.length && !isNaN(digit = getDigitCode(strNum, index, radix))) {
+            res = res * radix + digit;
+            index++;
+        }
+        if (strNum[0] == '-') {
+            res = -res;
+        }
+    }
+    return res;
+}
+
+function getDigitCode(strNum, index, radix) {
+    const delta = 'a'.charCodeAt(0) - 10;
+    const symbol = strNum.charAt(index).toLowerCase();
+    const code = symbol >= '0' && symbol <= '9' ? +symbol : symbol.charCodeAt(0) - delta;
+    return code >= 0 && code < radix ? code : NaN;
+}
+
+function getHexdecimalIndex(str) {
+    return str.toLowerCase().startsWith('0x') ? 2 : 0;
+}
