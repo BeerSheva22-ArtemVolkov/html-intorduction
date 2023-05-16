5ac165fee943652eba491e8c6fc0c8d0bbb54c2b
Art96em <artem-w0lf@yandex.ru>
2023-05-16 15:58:07 +0300

    functions / this / bind / call / apply

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-const x = { x: "x", toString: function () { return "x" } };
-const y = { y: "y", toString: function () { return "kuku" } };
-const d = { x: 10, y: 20 }
+this.x = 100;
+// console.log(this);
 
-x.toString = function () { return "xx" };
-d[x] = 100;
-d[y] = 200;
-const f = function () { };
-const num = 2;
-f.x = function (a, b) {
-    return a + b;
+function f1() {
+    return this;
+}
+const f2 = () => {
+    return this;
 }
 
-console.log(d);
-console.log(d[x]); // 100
-console.log(f.x()); // NaN
-console.log(f.x(10, 20)); // 30
+// console.log('f1 call result', f1());
+// console.log('f2 call result', f2());
+// console.log((() => {console.log(this)}));
 
-(2).x = 10;
-console.log((2).x); // undefind
+const x = {
+    f1: function () {
+        return this;
+    },
+    f2: () => { return this }
+}
 
-const ar = [2, 3];
-ar.x = 10;
-console.log(ar); // [2, 3, x : 10]
-console.log(ar.x); // 10
-console.log(Array.from({ length: 2 }));
-console.log(Array.from({length: 5}).map((__, index) => index + 5)); // [5, 6, 7, 8, 9]
-console.log(Array.from({length: 26})
-    .map((__, index) => String.fromCharCode(index + 'a'.charCodeAt(0)))
-    .map(s => `<div>${s}</div>`).join('')); // экранная клавиатура из div
+console.log(`x.f1 call result`, x.f1()); // передается "будто бы" x
+console.log(`x.f2 call result`, x.f2()); // у стрелочной функции нет своего this, поэтому берется глобальный
+
+const rectangle = {
+    width: 20, height: 20, square: function () {
+        return this.width * this.height
+    }, perimeter: () => { 2 * (this.width + this.height) }
+};
+
+console.log(`square = ${rectangle.square()}`);
+console.log(`perimeter = ${rectangle.perimeter()}`);
+
+const point = { x: 3, y: 4 }
+function displayPoint(z, t) {
+    console.log(`x = ${this.x}, y = ${this.y}, z = ${z}, t = ${t}`);
+}
+
+const displayPoint1 = displayPoint.bind(point, 100, 200); // прикрепляет this к displayPoint и возвращает (НЕ ВЫЗЫВАЕТ) "копию" с привязанным аргументами
+displayPoint1(9, -9); // параметры не выведутся на экран, тк эти значения уже заложены в объекте выше. У bind выше приоритет
+displayPoint.call(point, 200, 300);
+displayPoint.apply(point, 300, 400);
