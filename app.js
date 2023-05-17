84ccb5103c44b317575cc46bae8c678717eb4407
Art96em <artem-w0lf@yandex.ru>
2023-05-17 11:05:29 +0300

    JSON intorduction

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ console.log(`x.f2 call result`, x.f2()); // у стрелочной функци
 const rectangle = {
     width: 20, height: 20, square: function () {
         return this.width * this.height
-    }, perimeter: () => { 2 * (this.width + this.height) }
+    }, perimeter: () => 2 * (this.width + this.height)
+};
+
+const rectangle2 = {
+    width: 20, height: 20, square: function () {
+        return this.width * this.height
+    }, perimeter: () => 2 * (this.width + this.height)
 };
 
 console.log(`square = ${rectangle.square()}`);
@@ -39,4 +45,10 @@ function displayPoint(z, t) {
 const displayPoint1 = displayPoint.bind(point, 100, 200); // прикрепляет this к displayPoint и возвращает (НЕ ВЫЗЫВАЕТ) "копию" с привязанным аргументами
 displayPoint1(9, -9); // параметры не выведутся на экран, тк эти значения уже заложены в объекте выше. У bind выше приоритет
 displayPoint.call(point, 200, 300);
-displayPoint.apply(point, 300, 400);
+// displayPoint.apply(point, 300, 400);
+
+console.log(JSON.stringify(rectangle), JSON.stringify(rectangle2));
+console.log(`rectangle == rectangle2 is ${rectangle == rectangle2}`);
+console.log(`JSON.stringify(rectangle) == JSON.stringify(rectangle2) is ${JSON.stringify(rectangle) == JSON.stringify(rectangle2)}`);
+
+const rectangele3 = JSON.parse(JSON.stringify(rectangle)); // глубокая копия объекта со всеми уровнями
